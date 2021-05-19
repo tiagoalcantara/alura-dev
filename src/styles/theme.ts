7ab76ea5c573d1093ad76fe99f683aad8f43f093
Tiago Alcantara <tiagofacampos@gmail.com>
2021-05-18 23:43:43 -0300

    feat(theme): adds global style and theme config

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+export const defaultTheme = {
+    colors: {
+        lightBlue: "#96B9FD",
+        mediumBlue: "#7BA4FC",
+        blue: "#5081FB",
+        darkBlue: "#051D3B",
+    },
+    fontSize: {
+        title: "21px",
+        body: "16px",
+        sidebarTitle: "12px",
+    },
+    fonts: ["Inter"]
+}
